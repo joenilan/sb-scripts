dd241eb6fe63974a3803da9e2e84571e82218dd9
DreadedZombie <joenilan@gmail.com>
2026-08-15 14:23:57 -0400

    Fix WPF Expression namespace ambiguity

--- CRNTLY.StreamerBot.UI/ScriptHost/CrntlyScriptWindowBridge.cs
+++ CRNTLY.StreamerBot.UI/ScriptHost/CrntlyScriptWindowBridge.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Linq.Expressions;
+using Expression = System.Linq.Expressions.Expression;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Markup;
